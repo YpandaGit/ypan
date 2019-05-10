6da92ca27f7d0893bdd33d5247584e7e3de244a4
YpandaGit <1104732896@qq.com>
2019-05-10 22:13:30 +0800

    去除多余博客页面

--- site_config/blog.js
+++ site_config/blog.js
@@ -9,11 +9,11 @@ export default {
     postsTitle: '所有文章',
     list: [
       {
-        title: '手写Spring核心功能第一篇：IOC与DI',
+        title: 'IOC and DI',
         author: 'yPan',
         dateStr: '2019-05-10',
         desc: '手写Spring核心功能第一篇：IOC与DI',
-        link: '/zh-cn/blog/手写Spring核心功能第一篇：IOC与DI.html',
+        link: '/zh-cn/blog/blog1.html',
       }
     ],
   },
