30267bc5e80cd032ee6a8be1a87edd3ccb06e011
YpandaGit <1104732896@qq.com>
2019-05-11 15:46:39 +0800

    增加博客：Docsite集成Gitalk

--- site_config/blog.js
+++ site_config/blog.js
@@ -9,12 +9,19 @@ export default {
     postsTitle: '所有文章',
     list: [
       {
-        title: 'IOC and DI',
+        title: 'Docsite集成Gitalk',
+        author: 'yPan',
+        dateStr: '2019-05-11',
+        desc: 'Docsite集成Gitalk的方法',
+        link: '/zh-cn/blog/docsite/1.html',
+      },
+      {
+        title: '实现Spring的IOC/DI',
         author: 'yPan',
         dateStr: '2019-05-10',
         desc: '手写Spring核心功能第一篇：IOC与DI',
-        link: '/zh-cn/blog/blog1.html',
-      }
+        link: '/zh-cn/blog/java/1.html',
+      },
     ],
   },
 };
