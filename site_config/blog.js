8dc2eabbd812ab037dbc8d80cd2adbbb33372ab9
YpandaGit <1104732896@qq.com>
2019-05-13 15:03:51 +0800

    添加博客：SpringBoot接口返回数据统一将LONG转为String

--- site_config/blog.js
+++ site_config/blog.js
@@ -9,6 +9,13 @@ export default {
     postsTitle: '所有文章',
     list: [
       {
+        title: 'SpringBoot接口返回数据统一将LONG转为String',
+        author: 'yPan',
+        dateStr: '2019-05-13',
+        desc: '解决前端数字太大丢失精度的一种方法',
+        link: '/zh-cn/blog/java/2.html',
+      },
+      {
         title: 'Docsite集成Gitalk',
         author: 'yPan',
         dateStr: '2019-05-11',
